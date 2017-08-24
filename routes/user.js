f202a48e22291253eda83bd639b10a16116197e5
lgxin <595283048@q.com>
2017-08-24 18:05:25 +0800

    ddd

---
+++ routes/user.js
@@ -0,0 +1,13 @@
+/*
+* @Author: Marte
+* @Date:   2017-08-24 17:46:25
+* @Last Modified by:   Marte
+* @Last Modified time: 2017-08-24 17:46:33
+*/
+
+function User() {
+      this.name;
+      this.city;
+      this.age;
+}
+module.exports = User;
